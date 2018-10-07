0551cee8fe984c8c339754cdde9ca5bd92aeee8b
Tahakagit <francescodar@gmail.com>
2018-10-07 13:07:48 +0200

    fixing activity_filter ids

--- app/src/main/java/com/example/franc/mygest/fragments/MyDialogFragment.java
+++ app/src/main/java/com/example/franc/mygest/fragments/MyDialogFragment.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -93,7 +92,7 @@ public class MyDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_dialog, container, false);
+        View v = inflater.inflate(R.layout.fragment_menu_insert, container, false);
         return v;
     }
 
